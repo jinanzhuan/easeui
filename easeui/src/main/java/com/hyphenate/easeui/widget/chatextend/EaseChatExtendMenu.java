8f2b95127e977255b16683755f6f78b12dd204b5
neil <neil.liu@easemob.com>
2020-07-27 16:25:04 +0800

    add chat extend menu indicators.

--- easeui/src/main/java/com/hyphenate/easeui/widget/chatextend/EaseChatExtendMenu.java
+++ easeui/src/main/java/com/hyphenate/easeui/widget/chatextend/EaseChatExtendMenu.java
@@ -4,43 +4,42 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ConcatAdapter;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.hyphenate.easeui.EaseUI;
 import com.hyphenate.easeui.R;
-import com.hyphenate.easeui.adapter.EaseBaseChatExtendMenuAdapter;
 import com.hyphenate.easeui.adapter.EaseChatExtendMenuAdapter;
+import com.hyphenate.easeui.adapter.EaseChatExtendMenuIndicatorAdapter;
 import com.hyphenate.util.DensityUtil;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 /**
  * Extend menu when user want send image, voice clip, etc
  *
  */
-public class EaseChatExtendMenu extends RecyclerView {
+public class EaseChatExtendMenu extends FrameLayout implements PagingScrollHelper.onPageChangeListener {
     protected Context context;
+    private RecyclerView rvExtendMenu;
+    private RecyclerView rvIndicator;
     private List<ChatMenuItemModel> itemModels = new ArrayList<ChatMenuItemModel>();
     private EaseChatExtendMenuAdapter adapter;
     private int numColumns;
     private int numRows;
     private PagingScrollHelper helper;
     private EaseChatExtendMenuItemClickListener listener;
+    private EaseChatExtendMenuIndicatorAdapter indicatorAdapter;
 
     public EaseChatExtendMenu(Context context) {
         this(context, null);
@@ -53,6 +52,7 @@ public class EaseChatExtendMenu extends RecyclerView {
     public EaseChatExtendMenu(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(context, attrs);
+        initLayout();
     }
 
     private void init(Context context, AttributeSet attrs){
@@ -62,24 +62,47 @@ public class EaseChatExtendMenu extends RecyclerView {
         numRows = ta.getInt(R.styleable.EaseChatExtendMenu_numRows, 2);
         ta.recycle();
     }
+
+    private void initLayout() {
+        LayoutInflater.from(context).inflate(R.layout.ease_layout_chat_extend_menu, this);
+        rvExtendMenu = findViewById(R.id.rv_extend_menu);
+        rvIndicator = findViewById(R.id.rv_indicator);
+    }
+
     
     /**
      * init
      */
     public void init(){
+        initChatExtendMenu();
+        initChatExtendMenuIndicator();
+    }
+
+    private void initChatExtendMenu() {
         HorizontalPageLayoutManager manager = new HorizontalPageLayoutManager(numRows, numColumns);
         manager.setItemHeight(DensityUtil.dip2px(context, 90));
-        setLayoutManager(manager);
-        setHasFixedSize(true);
+        rvExtendMenu.setLayoutManager(manager);
+        rvExtendMenu.setHasFixedSize(true);
+        ConcatAdapter concatAdapter = new ConcatAdapter();
         adapter = new EaseChatExtendMenuAdapter();
+        concatAdapter.addAdapter(adapter);
+        rvExtendMenu.setAdapter(concatAdapter);
         adapter.setData(itemModels);
-        setAdapter(adapter);
 
         helper = new PagingScrollHelper();
-        helper.setUpRecycleView(this);
+        helper.setUpRecycleView(rvExtendMenu);
         helper.updateLayoutManger();
         helper.scrollToPosition(0);
         setHorizontalFadingEdgeEnabled(true);
+        helper.setOnPageChangeListener(this);
+    }
+
+    private void initChatExtendMenuIndicator() {
+        indicatorAdapter = new EaseChatExtendMenuIndicatorAdapter();
+        rvIndicator.setAdapter(indicatorAdapter);
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL);
+        itemDecoration.setDrawable(ContextCompat.getDrawable(context, R.drawable.ease_chat_extend_menu_indicator_divider));
+        rvIndicator.addItemDecoration(itemDecoration);
     }
 
     /**
@@ -101,6 +124,9 @@ public class EaseChatExtendMenu extends RecyclerView {
         item.id = itemId;
         item.clickListener = listener;
         itemModels.add(item);
+        adapter.notifyItemInserted(itemModels.size() - 1);
+        //设置需要显示的indicator的个数
+        indicatorAdapter.setPageCount((int) Math.ceil(itemModels.size() / (numColumns * numRows)));
     }
 
     /**
@@ -117,6 +143,12 @@ public class EaseChatExtendMenu extends RecyclerView {
      */
     public void registerMenuItem(int nameRes, int drawableRes, int itemId, EaseChatExtendMenuItemClickListener listener) {
         registerMenuItem(context.getString(nameRes), drawableRes, itemId, listener);
+    }
+
+    @Override
+    public void onPageChange(int index) {
+        //设置选中的indicator
+        indicatorAdapter.setSelectedPosition(index);
     }
 
     /**
