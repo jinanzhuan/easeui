40bc7611dd9b36a236698a6bdafe21dad1c2cc25
neil <neil.liu@easemob.com>
2020-08-10 19:24:50 +0800

    add EaseFileUtils.

--- easeui/src/main/java/com/hyphenate/easeui/utils/EaseCompat.java
+++ easeui/src/main/java/com/hyphenate/easeui/utils/EaseCompat.java
@@ -1,20 +1,13 @@
 package com.hyphenate.easeui.utils;
 
 import android.app.Activity;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.ProviderInfo;
 import android.content.res.Resources;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.WindowManager;
@@ -23,17 +16,10 @@ import android.widget.Toast;
 import com.hyphenate.easeui.R;
 import com.hyphenate.util.EMLog;
 import com.hyphenate.util.PathUtil;
-import com.hyphenate.util.UriUtils;
 import com.hyphenate.util.VersionUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.FileProvider;
@@ -121,17 +107,14 @@ public class EaseCompat {
      * @param uri
      */
     public static void openFile(Context context, Uri uri) {
-        String filePath = UriUtils.getFilePath(context, uri);
+        String filePath = EaseFileUtils.getFilePath(context, uri);
         //如果可以获取文件的绝对路径，则需要根据sdk版本处理FileProvider的问题
         if(!TextUtils.isEmpty(filePath) && new File(filePath).exists()) {
             openFile(context, new File(filePath));
             return;
         }
-        String filename = UriUtils.getFileNameByUri(context, uri);
-        String mimeType = UriUtils.getFileMimeType(context, uri);
-        if(TextUtils.isEmpty(mimeType) || TextUtils.equals(mimeType, "application/octet-stream")) {
-            mimeType = getMimeType(context, filename);
-        }
+        String filename = getFileNameByUri(context, uri);
+        String mimeType = getMimeType(context, filename);
         openFile(context, uri, filename, mimeType);
     }
 
@@ -168,21 +151,7 @@ public class EaseCompat {
      * @param uri
      */
     public static void deleteFile(Context context, Uri uri) {
-        if(UriUtils.isFileExistByUri(context, uri)) {
-            String filePath = UriUtils.getFilePath(context, uri);
-            if(!TextUtils.isEmpty(filePath)) {
-                File file = new File(filePath);
-                if(file != null && file.exists() && file.isFile()) {
-                    file.delete();
-                }
-            }else {
-                try {
-                    context.getContentResolver().delete(uri, null, null);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        EaseFileUtils.deleteFile(context, uri);
     }
 
     public static Uri getUriForFile(Context context, @NonNull File file) {
@@ -324,12 +293,12 @@ public class EaseCompat {
     }
 
     public static boolean openApk(Context context, Uri uri) {
-        String filename = UriUtils.getFileNameByUri(context, uri);
+        String filename = getFileNameByUri(context, uri);
         return openApk(context, uri, filename);
     }
 
     public static boolean openApk(Context context, Uri uri, @NonNull String filename) {
-        String filePath = UriUtils.getFilePath(context, uri);
+        String filePath = EaseFileUtils.getFilePath(context, uri);
         if(filename.endsWith(".apk")) {
             if(TextUtils.isEmpty(filePath) || !new File(filePath).exists()) {
                 Toast.makeText(context, "Can't find proper app to open this file", Toast.LENGTH_LONG).show();
@@ -373,6 +342,16 @@ public class EaseCompat {
     }
 
     /**
+     * 获取文件名
+     * @param context
+     * @param fileUri
+     * @return
+     */
+    public static String getFileNameByUri(Context context, Uri fileUri) {
+        return EaseFileUtils.getFileNameByUri(context, fileUri);
+    }
+
+    /**
      * Get a file path from a Uri. This will get the the path for Storage Access
      * Framework Documents, as well as the _data field for the MediaStore and
      * other file-based ContentProviders.
@@ -383,299 +362,7 @@ public class EaseCompat {
      */
     public static String getPath(final Context context, final Uri uri) {
 
-        return getFilePath(context, uri);
+        return EaseFileUtils.getFilePath(context, uri);
     }
 
-    /**
-     * Get a file path from a Uri. This will get the the path for Storage Access
-     * Framework Documents, as well as the _data field for the MediaStore and
-     * other file-based ContentProviders.
-     *
-     * @param context The context.
-     * @param uri     The Uri to query.
-     * @author paulburke
-     */
-    public static String getFilePath(final Context context, final Uri uri) {
-        if(uri == null) {
-            return "";
-        }
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-        //sdk版本在29之前的
-        if(!VersionUtils.isTargetQ(context)) {
-            // DocumentProvider
-            if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-                // ExternalStorageProvider
-                if (isExternalStorageDocument(uri)) {
-                    final String docId = DocumentsContract.getDocumentId(uri);
-                    final String[] split = docId.split(":");
-                    final String type = split[0];
-
-                    if ("primary".equalsIgnoreCase(type)) {
-                        return Environment.getExternalStorageDirectory() + "/" + split[1];
-                    }
-
-                    // TODO handle non-primary volumes
-                }
-                // DownloadsProvider
-                else if (isDownloadsDocument(uri)) {
-                    final String id = DocumentsContract.getDocumentId(uri);
-                    final Uri contentUri = ContentUris.withAppendedId(
-                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-
-                    return getDataColumn(context, contentUri, null, null);
-                }
-                // MediaProvider
-                else if (isMediaDocument(uri)) {
-                    final String docId = DocumentsContract.getDocumentId(uri);
-                    final String[] split = docId.split(":");
-                    final String type = split[0];
-
-                    Uri contentUri = null;
-                    if ("image".equals(type)) {
-                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("video".equals(type)) {
-                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("audio".equals(type)) {
-                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                    }
-
-                    final String selection = "_id=?";
-                    final String[] selectionArgs = new String[]{
-                            split[1]
-                    };
-
-                    return getDataColumn(context, contentUri, selection, selectionArgs);
-                }
-            }
-            else if(isFileProvider(context, uri)) {
-                return getFPUriToPath(context, uri);
-            }else if(isOtherFileProvider(context, uri)) {
-                return copyFileProviderUri(context, uri);
-            }
-            // MediaStore (and general)
-            else if (uriStartWithContent(uri)) {
-                return getDataColumn(context, uri, null, null);
-            }
-            // File
-            else if (uriStartWithFile(uri)) {
-                return uri.getPath();
-            }else if(uri.toString().startsWith("/")) {//如果是路径的话，返回路径
-                return uri.toString();
-            }
-        }else {
-            //29之后，判断是否是file开头及是否是以"/"开头
-            if(uriStartWithFile(uri)) {
-                return uri.getPath();
-            }else if(uri.toString().startsWith("/")) {
-                return uri.toString();
-            }
-        }
-        return "";
-    }
-
-    /**
-     * 从FileProvider获取文件
-     * @param context
-     * @param uri
-     * @return
-     */
-    private static String copyFileProviderUri(Context context, Uri uri) {
-        //如果是分享过来的文件，则将其写入到私有目录下
-        String[] subs = uri.toString().split("/");
-        String filename = null;
-        if(subs.length > 0) {
-            filename = subs[subs.length -1];
-        }else {
-            return "";
-        }
-        String filePath = PathUtil.getInstance().getFilePath() + File.separator + filename;
-        if(new File(filePath).exists()) {
-            return filePath;
-        }
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = context.getContentResolver().openInputStream(uri);
-            out = new FileOutputStream(filePath);
-            byte[] tmp = new byte[2048];
-            int l;
-            while ((l = in.read(tmp)) != -1) {
-                out.write(tmp, 0, l);
-            }
-            EMLog.d(TAG, "other fileProvider file path = "+filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if(in != null) {
-                    in.close();
-                }
-                if(out != null) {
-                    out.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return new File(filePath).exists() ? filePath : "";
-    }
-
-    /**
-     * 判断uri是否以file开头
-     * @param fileUri
-     * @return
-     */
-    public static boolean uriStartWithFile(Uri fileUri) {
-        return "file".equalsIgnoreCase(fileUri.getScheme()) && fileUri.toString().length() > 7;
-    }
-
-    /**
-     * 判断是否以content开头的Uri
-     * @param fileUri
-     * @return
-     */
-    public static boolean uriStartWithContent(Uri fileUri) {
-        return "content".equalsIgnoreCase(fileUri.getScheme());
-    }
-
-    /**
-     * Get the value of the data column for this Uri. This is useful for
-     * MediaStore Uris, and other file-based ContentProviders.
-     *
-     * @param context       The context.
-     * @param uri           The Uri to query.
-     * @param selection     (Optional) Filter used in the query.
-     * @param selectionArgs (Optional) Selection arguments used in the query.
-     * @return The value of the _data column, which is typically a file path.
-     */
-    public static String getDataColumn(Context context, Uri uri, String selection,
-                                       String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {
-                column
-        };
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-    /**
-     * 从FileProvider获取文件路径
-     * @param context
-     * @param uri
-     * @return
-     */
-    private static String getFPUriToPath(Context context, Uri uri) {
-        try {
-            List<PackageInfo> packs = context.getPackageManager().getInstalledPackages(PackageManager.GET_PROVIDERS);
-            if (packs != null) {
-                String fileProviderClassName = FileProvider.class.getName();
-                for (PackageInfo pack : packs) {
-                    ProviderInfo[] providers = pack.providers;
-                    if (providers != null) {
-                        for (ProviderInfo provider : providers) {
-                            if (uri.getAuthority().equals(provider.authority)) {
-                                if (provider.name.equalsIgnoreCase(fileProviderClassName)) {
-                                    Class<FileProvider> fileProviderClass = FileProvider.class;
-                                    try {
-                                        Method getPathStrategy = fileProviderClass.getDeclaredMethod("getPathStrategy", Context.class, String.class);
-                                        getPathStrategy.setAccessible(true);
-                                        Object invoke = getPathStrategy.invoke(null, context, uri.getAuthority());
-                                        if (invoke != null) {
-                                            String PathStrategyStringClass = FileProvider.class.getName() + "$PathStrategy";
-                                            Class<?> PathStrategy = Class.forName(PathStrategyStringClass);
-                                            Method getFileForUri = PathStrategy.getDeclaredMethod("getFileForUri", Uri.class);
-                                            getFileForUri.setAccessible(true);
-                                            Object invoke1 = getFileForUri.invoke(invoke, uri);
-                                            if (invoke1 instanceof File) {
-                                                String filePath = ((File) invoke1).getAbsolutePath();
-                                                return filePath;
-                                            }
-                                        }
-                                    } catch (NoSuchMethodException e) {
-                                        e.printStackTrace();
-                                    } catch (InvocationTargetException e) {
-                                        e.printStackTrace();
-                                    } catch (IllegalAccessException e) {
-                                        e.printStackTrace();
-                                    } catch (ClassNotFoundException e) {
-                                        e.printStackTrace();
-                                    }
-                                    break;
-                                }
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * 是否是本app的FileProvider
-     * @param context
-     * @param uri
-     * @return
-     */
-    public static boolean isFileProvider(Context context, Uri uri) {
-        return (context.getApplicationInfo().packageName + ".fileProvider").equalsIgnoreCase(uri.getAuthority());
-    }
-
-    /**
-     * 其他app分享过来的FileProvider
-     * @param context
-     * @param uri
-     * @return
-     */
-    public static boolean isOtherFileProvider(Context context, Uri uri) {
-        String scheme = uri.getScheme();
-        String authority = uri.getAuthority();
-        if(TextUtils.isEmpty(scheme) || TextUtils.isEmpty(authority)) {
-            return false;
-        }
-        return !(context.getApplicationInfo().packageName + ".fileProvider").equalsIgnoreCase(uri.getAuthority())
-                && "content".equalsIgnoreCase(uri.getScheme())
-                && authority.contains(".fileProvider".toLowerCase());
-    }
 }
